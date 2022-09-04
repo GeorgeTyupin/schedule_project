8065facee9970edb33d2bcedcb6d787ac8c1726e
GeorgeTyupin <georgytyupin@ya.ru>
2022-09-04 19:20:51 +0300

    Сделал авторизацию и регистарию и рендеринг событий у пользователя

--- schedule/core/database.py
+++ schedule/core/database.py
@@ -1,3 +1,4 @@
+import email
 from . import DATA_DST
 import sqlite3
 import time
@@ -6,16 +7,38 @@ class Database():
    def __init__(self):
       print('init database')
 
-   def addEvent(self, data):
-      with sqlite3.connect(DATA_DST) as cur:
-            sql = f"""INSERT INTO events ('name' , 'author_id', 'date', 'time', 'description') 
-               VALUES ('{data['event_name']}','1','{data['event_day']}','{time.time()}','{data['event-description']}')"""
-            cur.execute(sql)
-            cur.commit()
+   # def insert(self, table_name, data):
+   #    return f"""INSERT INTO {table_name} ({data['columns']}) 
+   #       VALUES ('{data["values"]}')"""
 
-   def loadEventsTable(self):
-        with sqlite3.connect(DATA_DST) as cur:
-            sql = f"SELECT * FROM events"
-            result = cur.execute(sql).fetchall()
-            return result
+   def addEvent(self, data, author_id):
+      with sqlite3.connect(DATA_DST) as cur:
+         # values = {}
+         sql = f"""INSERT INTO events ('name' , 'author_id', 'date', 'time', 'description') 
+            VALUES ('{data['event_name']}','{author_id}','{data['event_day']}','{time.time()}','{data['event-description']}')"""
+         # values['columns'] = 'name' , 'author_id', 'date', 'time', 'description'
+         # values["values"] = f"{data['event_name']}','{author_id}','{data['event_day']}','{time.time()}','{data['event-description']}"
+         # event = self.insert('events', values)
+         # cur.execute(event)
+         cur.execute(sql)
+         cur.commit()
+
+   def loadEventsTable(self, author_id):
+      with sqlite3.connect(DATA_DST) as cur:
+         sql = f"SELECT * FROM events WHERE author_id = {author_id}"
+         result = cur.execute(sql).fetchall()
+         return result
+   
+   def loadUsersTable(self, login):
+      with sqlite3.connect(DATA_DST) as cur:
+         sql = f"SELECT * FROM users WHERE user_name = '{login}' "
+         result = cur.execute(sql).fetchone()
+         return result
+   
+   def addUser(self, login, password, email):
+      with sqlite3.connect(DATA_DST) as cur:
+         sql = f"""INSERT INTO users ('user_name' , 'password', 'email') 
+            VALUES ('{login}','{password}','{email}')"""
+         cur.execute(sql)
+         cur.commit()
     
--- schedule/routes.py
+++ schedule/routes.py
@@ -8,16 +8,57 @@ db = core.database.Database()
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        return render_template('adminlk.html')
+        if session:
+            return render_template('adminlk.html')
+        else:
+            return render_template('auth.html')
     else:
         data = {}
         data['event_name'] = request.form.get('event_name')
         data['event_day'] = request.form.get('event_day')
         data['event-description'] = request.form.get('event-description')
         print(data)
-        db.addEvent(data)
+        db.addEvent(data, session['id'])
         return ''
+
+@app.route('/auth', methods=['GET', 'POST'])
+def auth():
+    if request.method == 'GET':
+        return render_template('auth.html')
+    else:
+        login = request.form.get('user_name')
+        password = request.form.get('user_password')
+        result = db.loadUsersTable(login)
+        if (result and result[2] == password):
+            session['id'] = result[0]
+            session['login'] = result[1]
+            session['email'] = result[3]
+            session['auth'] = True
+            response = make_response(redirect(f"/"))
+            return response
+        else:
+            return render_template('auth.html')
+
+@app.route('/reg', methods=['GET', 'POST'])
+def reg():
+    if request.method == 'GET':
+        return render_template('reg.html')
+    else:
+        login = request.form.get('user_name')
+        email = request.form.get('user_email')
+        password = request.form.get('user_password')
+        result = db.loadUsersTable(login)
+        if result:
+            return "Такой пользователь уже существует"
+        db.addUser(login, password, email)
+        result = db.loadUsersTable(login)
+        session['id'] = result[0]
+        session['login'] = login
+        session['email'] = result[3]
+        session['auth'] = True
+        response = make_response(redirect("/"))
+        return response
 
 @app.route("/getdata", methods=['GET', 'POST'])
 def getdata():
-    return json.dumps(db.loadEventsTable())
+    return json.dumps(db.loadEventsTable(session['id']))
